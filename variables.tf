3bb0b1ec5fa0b6812c7d10898ac79a4115e207ac
asaf9879 <asaf9879@gmail.com>
2025-07-31 10:49:22 +0300

    Initial commit without .terraform

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+  default     = "my-eks-cluster"
+}
